fef6803b3f47399f975852df88a6513c9ea270c7
charles <charles.nikdel@gmail.com>
2023-04-12 08:46:39 +0200

    consignes

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,11 @@
 char	*get_next_line(int fd)
 {
+    /* Prendre retour a la ligne
+    le read garde pointeur meme endroit
+    stocker qq part a chaque lecture
+    renvoie qd newline AVEC la newline
+    du coup => le stock doit etre static
+    puis nettoyer par la suite*/
+
+
 
