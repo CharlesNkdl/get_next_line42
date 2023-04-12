e98f676e140638f379f0f1237744f74a3d0f5c3d
charles <charles.nikdel@gmail.com>
2023-04-12 22:38:34 +0200

    reprise a zero

--- get_next_line.c
+++ get_next_line.c
@@ -1,11 +1,5 @@
 #include "get_next_line.h"
 
-typedef struct s_list
-{
-	char			*content;
-	struct s_llist	*next;
-}			t_list;
-
     /* Prendre retour a la ligne
     le read garde pointeur meme endroit
     stocker qq part a chaque lecture
@@ -26,96 +20,4 @@ typedef struct s_list
 
 char	*get_next_line(int fd)
 {
-    static t_list *queue = NULL;
-    char *stack;
-
-    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &stack, 0) < 0)
-        return (NULL);
-
-
-
-
-
-
 }
-/* Avec ft_filling, j'obtiens du coup, normalement, forcement une ligne complete mais pas encore trimmer du surplus avec la newline
-    tout ca stocker dans une linked list qui est lie a la list static dans get next line*/
-/*void    ft_filling(int fd, t_list **queue)
-{
-    char *stack;
-    int check;
-
-    check = 1;
-    while (ft_strchr(*stack, 10) == 0 && check != 0)
-    {
-        stack = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-        if (!stack)
-            return ;
-        check = (int)read(fd, stack, BUFFER_SIZE);
-        if ((!*stack && check == 0) || check == -1)
-        {
-            free(stack);
-            return ;
-        }
-        stack[check] = '\0';
-        ft_strcpyllback(queue, stack, check);
-        free(stack);
-    }
-}
-
-void    ft_strcpyllback(t_list **queue, char *stack, int check)
-{
-    int i;
-    t_list  *last;
-    t_list  *new;
-
-    new = malloc(sizeof(t_list));
-    if (!new)
-        return ;
-    new->next = NULL;
-    new->content = malloc(sizeof(char) * check + 1);
-    if (!(new->content))
-        return ;
-    i = 0;
-    while (stack[i] && i < check)
-    {
-        new->content[i] = stack[i];
-        i++;
-    }
-    new->content[i] = '\0';
-    if (!(*queue))
-    {
-        *queue = new;
-        return ;
-    }
-    last = ft_lstlast(*queue);
-    last->next = new;
-}*/
-
-char    *ft_strchr(char const *s, int c)
-{
-    char    find;
-    int         i;
-
-    find = (unsigned char)c;
-    i = 0;
-    while(s[i])
-    {
-        if (s[i] == find)
-            return ((char *)s + i);
-        i++;
-    }
-    if (s[i] == find)
-        return ((char *)s + i);
-    return (0);
-}
-
-
-
-
-int main(int argc, char **argv)
-{
-
-}
-
-
